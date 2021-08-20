e0b094aaeb662d91a9d2d6c9998370b75e4c4192
Eileen Olivera <eileenolivera@hotmail.com>
2021-08-20 16:47:53 -0400

    rendered browser data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,52 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios"
+import { Route, Link } from "react-router-dom";
+
 
 export default function App() {
+const [chores, setChores] = useState([])
+
+
+  useEffect(() => {
+    const fetchChores = async () => {
+      const resp = await axios(`https://api.airtable.com/v0/appFGm05ROt4K1hdH/Table%201`,
+        {
+          headers: {
+            Authorization: `Bearer keyecAOfQ6CfXYswX`
+          }
+        }
+      )
+      console.log(resp.data.records)
+      setChores(resp.data.records)
+    }
+    fetchChores()
+  }, [])
+
   return (
     <>
-    <nav>
+      <nav>
       <Link to="/">Home</Link>
-      <Link to="/new">Add Chore</Link>
+      <Link to="/new">Add A Chore</Link>
     </nav>
-      <Route path="/" exact></Route>
+      <Route path="/" exact>
+        {chores.map((chore, index) => {
+            const {name, description} = chore.fields
+            return (
+              <article key={index}>
+                <h3>{name}</h3>
+                <p>{description}</p>
+              </article>
+            )
+          })
+        }
+      </Route>
+      <Route path="/new">
+        <h1>Create new chore form.</h1>
+      </Route>
+      <Route path="/edit/:id">
+        <h1>Pre-populated chore to edit.</h1>
+      </Route>
     </>
   );
 }
